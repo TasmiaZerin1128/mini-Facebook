ae654cae72bf13b98721377da00c46f523095e0b
TasmiaZerin1128 <ttaosnmniya.6638.cross@gmail.com>
2022-09-08 11:18:05 +0600

    added alpine

--- backend/authentication/server.js
+++ backend/authentication/server.js
@@ -13,8 +13,12 @@ dotenv.config();
 
 
 //connect to DB
-mongoose.connect(process.env.DB_CONNECT_LOCAL, { useNewUrlParser: true },() => 
+mongoose.connect(process.env.DB_CONNECT_LOCAL, { useNewUrlParser: true },(err, client) => {
+    if (err) {
+        return console.log('Cannot connect '+ err);
+    }
     console.log('Connected to DB')
+}
 );
 
 //Middlewares
